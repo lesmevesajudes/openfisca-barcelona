7b7aa24041d980543a88979c1c8c0e2333176b52
Joan Valduvieco <jvalduvieco@gmail.com>
2017-09-15 14:53:20 +0200

    Prepare to use yaml parameters

--- openfisca_spain/spain_taxbenefitsystem.py
+++ openfisca_spain/spain_taxbenefitsystem.py
@@ -13,11 +13,5 @@ class SpainTaxBenefitSystem(TaxBenefitSystem):
         # We add to our tax and benefit system all the variables
         self.add_variables_from_directory(os.path.join(COUNTRY_DIR, 'variables'))
 
-        # We add to our tax and benefit system all the legislation parameters defined in the  parameters files
-        param_files = [
-            '__root__.xml',
-        ]
-
-        for param_file in param_files:
-            param_path = os.path.join(COUNTRY_DIR, 'parameters', param_file)
-            self.add_legislation_params(param_path)
+        param_path = os.path.join(COUNTRY_DIR, 'parameters')
+        self.load_parameters(param_path)
