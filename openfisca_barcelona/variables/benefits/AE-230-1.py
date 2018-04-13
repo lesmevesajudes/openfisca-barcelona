a73e60747591d3f89bc967e0d34e4bd53c765005
jvalduvieco <jvalduvieco@gmail.com>
2018-04-13 12:14:28 +0200

    Calculate AE-230-1 correctly in case of monoparental

--- openfisca_barcelona/variables/benefits/AE-230-1.py
+++ openfisca_barcelona/variables/benefits/AE-230-1.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# This file defines the variables of our legislation.
-# A variable is property of a person, or an entity (e.g. a familia).
-# See https://doc.openfisca.fr/variables.html
-
-# Import from openfisca-core the common python objects used to code the legislation in OpenFisca
-from datetime import datetime
 from openfisca_core.model_api import *
-# Import the entities specifically defined for this tax and benefit system
 from openfisca_barcelona.entities import *
 from openfisca_barcelona.variables.demographics import TIPUS_FAMILIA_MONOPARENTAL
+
 
 class AE_230_01_mensual(Variable):
     column = IntCol(val_type="monetary")
@@ -18,9 +12,13 @@ class AE_230_01_mensual(Variable):
     label = "Ajuda 0-16"
 
     def formula(persona, period, parameters):
-        import_ajuda = select([persona.familia('tipus_familia_monoparental', period)[0] == TIPUS_FAMILIA_MONOPARENTAL['General'],
-                                persona.familia('tipus_familia_monoparental', period)[0] == TIPUS_FAMILIA_MONOPARENTAL['Especial'],
+        es_monoparental = (persona.familia('tipus_familia_monoparental', period)[0] == TIPUS_FAMILIA_MONOPARENTAL['General']) \
+                          + (persona.familia('tipus_familia_monoparental', period)[0] == TIPUS_FAMILIA_MONOPARENTAL['Especial'])
+        es_monoparental_custodia_total = es_monoparental * (persona('tipus_custodia', period) == 'total')
+        es_monoparental_custodia_compartida = es_monoparental * (persona('tipus_custodia', period) == 'compartida')
+        import_ajuda = select([es_monoparental_custodia_total,
+                               es_monoparental_custodia_compartida,
                                 persona.familia('tipus_familia_monoparental', period)[0] == TIPUS_FAMILIA_MONOPARENTAL['No']],
-                               [900, 900, 0])
+                               [900, 450, 0])
 
         return persona('compleix_criteris_AE230', period) * import_ajuda
