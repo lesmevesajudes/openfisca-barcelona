4d918b703a63b097f9079a910982851a31869f3c
jvalduvieco <jvalduvieco@gmail.com>
2018-07-02 00:56:49 +0200

    Add MIFO benefits

---
+++ openfisca_barcelona/variables/benefits/HA-005.py
@@ -0,0 +1,75 @@
+from openfisca_core.model_api import *
+from openfisca_barcelona.entities import *
+from openfisca_barcelona.variables.benefits.HA import clauIRSCPonderat, clauMultiplicadors
+
+
+class lloguer_inferior_al_maxim_per_demarcacio_HA005(Variable):
+    value_type = bool
+    entity = UnitatDeConvivencia
+    definition_period = MONTH
+    label = "Some person has a familiar relation to the owner"
+    default_value = False
+
+    def formula(unitatDeConvivencia, period, legislation):
+        import_del_lloguer = unitatDeConvivencia("import_del_lloguer", period)
+        demarcacio_de_lhabitatge = unitatDeConvivencia("demarcacio_de_lhabitatge", period)
+        lloguer_maxim_per_demarcacio = legislation(period).benefits.HA001.import_lloguer_maxim["HA005"][demarcacio_de_lhabitatge]
+        return import_del_lloguer < lloguer_maxim_per_demarcacio
+
+
+class pot_ser_solicitant_HA005(Variable):
+    value_type = bool
+    entity = Persona
+    definition_period = MONTH
+    label = "Is this person suitable to apply for this benefit"
+    default_value = False
+
+    def formula(persona, period, legislation):
+        tipus_document_identitat = persona("tipus_document_identitat", period)
+        has_DNI = tipus_document_identitat == tipus_document_identitat.possible_values.DNI
+        has_NIE = tipus_document_identitat == tipus_document_identitat.possible_values.NIE
+        empadronat_a_barcelona = persona("municipi_empadronament", period) == "barcelona"
+        temps_empadronat_a_lhabitatge = persona("temps_empadronat_habitatge_actual", period)
+        empadronat_a_lhabitatge = temps_empadronat_a_lhabitatge != temps_empadronat_a_lhabitatge.possible_values.no_empadronat
+        titular_contracte_de_lloguer = persona("titular_contracte_de_lloguer", period)
+        return (has_DNI + has_NIE) \
+               * empadronat_a_barcelona \
+               * empadronat_a_lhabitatge \
+               * titular_contracte_de_lloguer
+
+
+class HA_005(Variable):
+    value_type = float
+    unit = 'currency'
+    entity = UnitatDeConvivencia
+    definition_period = MONTH
+    label = u"SUBVENCIONS DE TIPUS MIFO"
+
+    def formula(unitatDeConvivencia, period, legislation):
+        nr_membres = unitatDeConvivencia.nb_persons()
+        discapacitats = unitatDeConvivencia.members("grau_discapacitat", period)
+        existeix_algun_discapacitat = unitatDeConvivencia.any(discapacitats)
+        zona_de_lhabitatge = unitatDeConvivencia("zona_de_lhabitatge", period)
+        poden_solicitar = unitatDeConvivencia.members("pot_ser_solicitant_HA005", period)
+        existeix_solicitant_viable = unitatDeConvivencia.any(poden_solicitar)
+        ingressos_bruts = unitatDeConvivencia.members("ingressos_bruts", period.last_year)
+        ingressos_familia_mensuals = unitatDeConvivencia.sum(ingressos_bruts) / 12
+        nivell_ingressos_maxim = \
+            legislation(period).benefits.HA001.irsc_ponderat[zona_de_lhabitatge][clauIRSCPonderat(nr_membres)] \
+            * legislation(period).benefits.HA001.multiplicadors[
+                clauMultiplicadors(nr_membres, existeix_algun_discapacitat)]
+        ingressos_bruts_dins_barems = ingressos_familia_mensuals < nivell_ingressos_maxim
+        lloguer_inferior_al_maxim_per_demarcacio = unitatDeConvivencia("lloguer_inferior_al_maxim_per_demarcacio_HA005", period)
+        no_es_ocupant_dun_habitatge_gestionat_per_lagencia_de_lhabitatge = \
+            unitatDeConvivencia("es_ocupant_dun_habitatge_gestionat_per_lagencia_de_lhabitatge", period) == False
+        no_tinc_alguna_propietat_a_part_habitatge_habitual_i_disposo_dusdefruit = \
+            unitatDeConvivencia("tinc_alguna_propietat_a_part_habitatge_habitual_i_disposo_dusdefruit", period) == False
+        no_relacio_de_parentiu_amb_el_propietari = \
+            unitatDeConvivencia("relacio_de_parentiu_amb_el_propietari", period) == False
+
+        return existeix_solicitant_viable \
+               * ingressos_bruts_dins_barems \
+               * lloguer_inferior_al_maxim_per_demarcacio \
+               * no_es_ocupant_dun_habitatge_gestionat_per_lagencia_de_lhabitatge \
+               * no_tinc_alguna_propietat_a_part_habitatge_habitual_i_disposo_dusdefruit \
+               * no_relacio_de_parentiu_amb_el_propietari
