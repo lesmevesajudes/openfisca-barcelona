4e8021d17ae6fad17d7f45e29cb1a77b1cba87b3
jvalduvieco <jvalduvieco@gmail.com>
2018-07-18 18:29:00 +0200

    AE-230-1 is granted to parents

--- openfisca_barcelona/variables/benefits/AE-230-1.py
+++ openfisca_barcelona/variables/benefits/AE-230-1.py
@@ -12,15 +12,17 @@ class AE_230_01_mensual(Variable):
     label = "Ajuda 0-16"
 
     def formula(persona, period, parameters):
+        is_adult = persona.has_role(Familia.ADULT)
         tipus_monoparental = persona.familia('tipus_familia_monoparental', period)
         es_monoparental = tipus_monoparental != tipus_monoparental.possible_values.nop
         tipus_custodia = persona.familia('tipus_custodia', period)
         es_monoparental_custodia_total = es_monoparental * (tipus_custodia == tipus_custodia.possible_values.total)
         es_monoparental_custodia_compartida = es_monoparental * (tipus_custodia == tipus_custodia.possible_values.compartida)
-
+        compleix_criteris_AE230 = persona.familia.members('compleix_criteris_AE230', period)
+        algun_membre_compleix_criteris_AE230 = persona.familia.any(compleix_criteris_AE230)
         import_ajuda = select([es_monoparental_custodia_total,
                                es_monoparental_custodia_compartida,
                                tipus_monoparental == tipus_monoparental.possible_values.nop],
                                [900, 450, 0])
 
-        return persona('compleix_criteris_AE230', period) * import_ajuda
+        return algun_membre_compleix_criteris_AE230 * is_adult * import_ajuda
