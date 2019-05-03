0699e6f7c4d3aa286461fd819ccd000ae87616f6
Martin Deluca <martin.deluca@jamgo.es>
2019-05-03 14:22:33 +0200

    vincles A-B

--- openfisca_barcelona/variables/benefits/GA-234-02.py
+++ openfisca_barcelona/variables/benefits/GA-234-02.py
@@ -9,23 +9,26 @@ def clauNombreDeMebres(membres):
     return select([membres == 1, membres == 2, membres == 3, membres >= 4],
                   ['1', '2', '3', '4'])
 
-class GA_234_01(Variable):
+class GA_234_02(Variable):
     value_type = float
     unit = 'currency'
     entity = Persona
     definition_period = MONTH
-    label = "GA_234_01 - Ajuda Vincles (A)"
+    label = "GA_234_02 - Ajuda Vincles (B)"
 
     def formula(persona, period, parameters):
         requeriments_generals = persona('GA_234', period)
         
         ingressos_bruts_familia = persona.familia('familia_ingressos_bruts', period.last_year) 
         nr_membres = persona.familia_fins_a_segon_grau.nb_persons()
-        llindar_ingressos = parameters(period).benefits.GA234.llindars_ingressos['A'][clauNombreDeMebres(nr_membres)]
-        compleix_nivell_ingressos = ingressos_bruts_familia <= llindar_ingressos
+        llindar_ingressos_minims = parameters(period).benefits.GA234.llindars_ingressos['A'][clauNombreDeMebres(nr_membres)]
+        llindar_ingressos = parameters(period).benefits.GA234.llindars_ingressos['B'][clauNombreDeMebres(nr_membres)]
+        compleix_ingressos_minims = ingressos_bruts_familia > llindar_ingressos_minims
+        compleix_ingressos_maxims = ingressos_bruts_familia <= llindar_ingressos
         
         compleix_els_requeriments = \
             requeriments_generals \
-            * compleix_nivell_ingressos
+            * compleix_ingressos_minims \
+            * compleix_ingressos_maxims
 
         return compleix_els_requeriments
