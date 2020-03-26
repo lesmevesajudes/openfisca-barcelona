6c33618503cfa5faa801c7a01a099ecd75e7bd14
Martin Espinach <martin.espinach@jamgo.coop>
2020-03-26 15:01:32 +0100

    fix: some dependency versions fixed

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ setup(
     },
     include_package_data=True,  # Will read MANIFEST.in
     install_requires=[
+        'gunicorn >=19.7.1, <20.0.0',
+        'Werkzeug >=0.14.0, <1.0.0',
         'OpenFisca-Core[web-api] >=31.0, <32.0',
     ],
     extras_require={
