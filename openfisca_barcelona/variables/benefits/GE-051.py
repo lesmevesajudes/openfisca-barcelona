240de8db352f02ade2d17720122785f41403ee6e
Martin Deluca <martin.deluca@jamgo.coop>
2021-02-20 20:57:14 +0100

    fix: hardcoded RAI max income limit

--- openfisca_barcelona/variables/benefits/GE-051.py
+++ openfisca_barcelona/variables/benefits/GE-051.py
@@ -3,27 +3,28 @@ from openfisca_core.model_api import *
 from openfisca_barcelona.entities import *
 
 
-class renda_mensual_disponible_inferior_a_530(Variable):
+class renda_mensual_disponible_inferior_a_75_percent_SMI(Variable):
     value_type = bool
     entity = Persona
     definition_period = MONTH
-    label = "Available income is under 530 euros/month"
+    label = "Available income is under 75 percent of IMS"
     set_input = set_input_dispatch_by_period
 
-    def formula(persona, period, legislation):
-        return persona('ingressos_bruts', period.last_year)/12 <= 551.93
+    def formula(persona, period, parameters):
+        llindar_ingressos = parameters(period).benefits.GE051.llindars_ingressos
+        return persona('ingressos_bruts', period.last_year)/12 <= llindar_ingressos
 
 
-class cap_familiar_te_renda_disponible_superior_a_530(Variable):
+class cap_familiar_te_renda_disponible_superior_a_75_percent_SMI(Variable):
     value_type = bool
     entity = Familia
     definition_period = MONTH
-    label = "No one is making more than 530 euros/month"
+    label = "No one is making more than 75 percent of IMS"
     set_input = set_input_dispatch_by_period
 
     def formula(familia, period, legislation):
-        members_renda_disponible_inferior_a_530 = familia.members('renda_mensual_disponible_inferior_a_530', period)
-        return familia.all(members_renda_disponible_inferior_a_530)
+        members_renda_disponible_inferior_a_75_percent_SMI = familia.members('renda_mensual_disponible_inferior_a_75_percent_SMI', period)
+        return familia.all(members_renda_disponible_inferior_a_75_percent_SMI)
 
 class ha_estat_beneficiari_de_la_rai_en_els_ultims_12_mesos(Variable):
     value_type = bool
@@ -94,8 +95,8 @@ class GE_051_mensual(Variable):
     label = "GE_051_1 - RAI 1 - Ajuda discapacitats 33% o superior"
 
     def formula(persona, period, parameters):
-        cap_membre_amb_ingressos_superiors_a_530_mensuals = \
-            persona.familia('cap_familiar_te_renda_disponible_superior_a_530', period)
+        cap_membre_amb_ingressos_superiors_a_75_percent_SMI = \
+            persona.familia('cap_familiar_te_renda_disponible_superior_a_75_percent_SMI', period)
         situacio_laboral = persona('situacio_laboral', period)
         aturat = situacio_laboral == situacio_laboral.possible_values.aturat
         no_se_li_ha_concedit_cap_ajuda_rai_en_els_ultims_12_mesos = \
@@ -109,7 +110,7 @@ class GE_051_mensual(Variable):
         menor_de_65_anys = persona('edat', period) < 65
 
         return \
-            cap_membre_amb_ingressos_superiors_a_530_mensuals \
+            cap_membre_amb_ingressos_superiors_a_75_percent_SMI \
             * aturat \
             * menor_de_65_anys \
             * no_treballa_per_compte_propi \
