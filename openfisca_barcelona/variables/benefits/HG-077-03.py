ec3e59fa637b18f9b65c987a6b4772ae25ff1182
Martin Deluca <martin.deluca@jamgo.coop>
2021-11-23 22:50:08 +0100

    feature: update conditions for HG_077_03

--- openfisca_barcelona/variables/benefits/HG-077-03.py
+++ openfisca_barcelona/variables/benefits/HG-077-03.py
@@ -1,7 +1,6 @@
 from openfisca_core.model_api import *
 from openfisca_barcelona.entities import *
-from openfisca_barcelona.variables.benefits.H import clauIRSCPonderat, clauMultiplicadors
-
+from openfisca_barcelona.variables.benefits.H import clauIRSCPonderat, clauMultiplicadors, demarcacioDeLHabitatge, clauNombreDeMebres, clauDependencia
 
 class quota_hipoteca_inferior_al_maxim_per_demarcacio_HG_077_03(Variable):
     value_type = bool
@@ -28,21 +27,17 @@ class pot_ser_solicitant_HG_077_03(Variable):
         tipus_document_identitat = persona("tipus_document_identitat", period)
         has_DNI = tipus_document_identitat == tipus_document_identitat.possible_values.DNI
         has_NIE = tipus_document_identitat == tipus_document_identitat.possible_values.NIE
-        empadronat_a_catalunya = (persona("municipi_empadronament", period) == b'barcelona') \
-        + (persona("municipi_empadronament", period) == b'altres') \
-        + (persona("municipi_empadronament", period) == b'municipis_atm')
         temps_empadronat_a_lhabitatge = persona("temps_empadronat_habitatge_actual", period)
         empadronat_a_lhabitatge = temps_empadronat_a_lhabitatge != temps_empadronat_a_lhabitatge.possible_values.no_empadronat
         titular_hipoteca = persona("titular_hipoteca", period)
 
         return (has_DNI + has_NIE) \
-               * empadronat_a_catalunya \
                * empadronat_a_lhabitatge \
                * titular_hipoteca
 
 
 class HG_077_03(Variable):
-    value_type = float
+    value_type = bool
     unit = 'currency'
     entity = UnitatDeConvivencia
     definition_period = MONTH
@@ -50,17 +45,22 @@ class HG_077_03(Variable):
 
     def formula(unitatDeConvivencia, period, legislation):
         nr_membres = unitatDeConvivencia.nb_persons()
+        resident_a_barcelona_ciutat = \
+            unitatDeConvivencia("demarcacio_de_lhabitatge", period) == demarcacioDeLHabitatge.barcelona_ciutat
         discapacitats = unitatDeConvivencia.members("grau_discapacitat", period)
         existeix_algun_discapacitat = unitatDeConvivencia.any(discapacitats)
-        zona_de_lhabitatge = unitatDeConvivencia("zona_de_lhabitatge", period)
         poden_solicitar = unitatDeConvivencia.members("pot_ser_solicitant_HG_077_03", period)
         existeix_solicitant_viable = unitatDeConvivencia.any(poden_solicitar)
         ingressos_bruts = unitatDeConvivencia.members("ingressos_bruts_ultims_sis_mesos", period)
         ingressos_familia_mensuals = unitatDeConvivencia.sum(ingressos_bruts) / 6 / nr_membres
+
+        discapacitats = unitatDeConvivencia.members("grau_discapacitat", period) > 33
+        dependencies = unitatDeConvivencia.members("grau_dependencia", period) == 3
+        existeix_discapacitat_o_dependencia = unitatDeConvivencia.any(discapacitats) + unitatDeConvivencia.any(dependencies)
         nivell_ingressos_maxim = \
-            legislation(period).benefits.H.irsc_ponderat[zona_de_lhabitatge][clauIRSCPonderat(nr_membres)] \
-            * legislation(period).benefits.H.multiplicadors[clauMultiplicadors(nr_membres, existeix_algun_discapacitat)]
+            legislation(period).benefits.HG07704.ingressos_maxims[clauDependencia(existeix_discapacitat_o_dependencia)][clauNombreDeMebres(nr_membres)]
         ingressos_bruts_dins_barems = ingressos_familia_mensuals <= nivell_ingressos_maxim
+
         ha_pagat_12_mesos_daquesta_hipoteca = unitatDeConvivencia("ha_pagat_12_mesos_daquesta_hipoteca", period)
         no_es_ocupant_dun_habitatge_gestionat_per_lagencia_de_lhabitatge = \
             unitatDeConvivencia("es_ocupant_dun_habitatge_gestionat_per_lagencia_de_lhabitatge", period) == False
@@ -69,13 +69,12 @@ class HG_077_03(Variable):
         no_relacio_de_parentiu_amb_el_propietari = \
             unitatDeConvivencia("relacio_de_parentiu_amb_el_propietari", period) == False
         quota_hipoteca_inferior_al_maxim_per_demarcacio_HG_077_03 = unitatDeConvivencia("quota_hipoteca_inferior_al_maxim_per_demarcacio_HG_077_03", period)
-        import_ajuda = min(3000, unitatDeConvivencia("import_deute_en_el_pagament_hipoteca", period))
 
         return existeix_solicitant_viable \
+               *resident_a_barcelona_ciutat \
                * ingressos_bruts_dins_barems \
                * ha_pagat_12_mesos_daquesta_hipoteca \
                * no_es_ocupant_dun_habitatge_gestionat_per_lagencia_de_lhabitatge \
                * no_tinc_alguna_propietat_a_part_habitatge_habitual_i_disposo_dusdefruit \
                * no_relacio_de_parentiu_amb_el_propietari \
-               * quota_hipoteca_inferior_al_maxim_per_demarcacio_HG_077_03 \
-               * import_ajuda
+               * quota_hipoteca_inferior_al_maxim_per_demarcacio_HG_077_03
